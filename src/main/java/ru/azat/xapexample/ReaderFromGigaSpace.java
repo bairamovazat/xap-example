d9ff236b95857b1a837c98244691f1f6f89facca
Mariya Pronina <mpronina@mera.ru>
2020-03-02 11:33:40 +0300

    Добавил в зависимости 10 gigaspace

--- src/main/java/ru/azat/xapexample/ReaderFromGigaSpace.java
+++ src/main/java/ru/azat/xapexample/ReaderFromGigaSpace.java
@@ -14,7 +14,7 @@ public class ReaderFromGigaSpace {
     public static void main(String[] args) {
         Thread thread = new Thread(() -> {
             try {
-                GigaSpace space = getGigaSpace("/./simple-space?schema=default&groups=xap-15.2.0&state=started");
+                GigaSpace space = getGigaSpace("jini://*/*/myGrid?groups=You_Can_Wipe_This&ignoreValidation=true&total_members=2,1&cluster_schema=partitioned-sync2backup&id=2&schema=default&state=started");
 
                 for (int i = 0; i < 100; i++) {
                     int count = space.count(new Event());
